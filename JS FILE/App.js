b6066778c69e3d11aab7fef533e80a4fbb9df9f2
amirhossainbond <67521933+amirhossainbond@users.noreply.github.com>
2022-10-17 19:43:04 +0600

    news_modal_set

--- JS FILE/App.js
+++ JS FILE/App.js
@@ -8,13 +8,14 @@ const loadNewsCatagory= ()=>{
 
 const displayCategory= (categories)=>{
 for(const category of categories){
-    console.log(category.category_name)
+    // console.log(category.category_name)
     const Did=(`${category.category_id}`)
-    //  fetch(url).then(res=>res.json()).then(data=>console.log(category.category_id))
+
 
 const catagoryContainer= document.getElementById('catagory')
     const newCatagoryDIv= document.createElement('div')
     newCatagoryDIv.classList.add('topnav')
+    
     
     newCatagoryDIv.innerHTML=`
 
@@ -22,7 +23,7 @@ const catagoryContainer= document.getElementById('catagory')
 `
 catagoryContainer.appendChild(newCatagoryDIv)
 }
-         
+
 
 }
 
@@ -43,7 +44,7 @@ const categorynewsList= (categories)=>{
     console.log(categories.length)
     newsContainer.innerHTML="";
  for(category of categories){
-    console.log(category)
+    // console.log(category._id)
   const newsAmount= document.getElementById('newsAmount')
         newsAmount.innerText='';
         newsAmount.innerText= `   Total News ${categories.length} 
@@ -53,19 +54,21 @@ const categorynewsList= (categories)=>{
     
 
     newNewsDiv.innerHTML=`
-    <div class="card m-2" style="width: 15rem;">
+    <div class="card m-2" style="width: 22rem;">
     <img src=${category.image_url} class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${category.title}</h5>
-      <p class="card-text ">${category.details.slice(0,100)}</p>
+      <p class="card-text ">${category.details.slice(0,100)}  ...</p>
+      <button onclick="openNav('${category._id}')" type="button" class="btn btn-success">Read More</button>
     </div>
     <div class="card-body">
-    <i class="fa-sharp fa-solid fa-eye"></i> ${category.total_view}
-    <button onclick="openNav()" type="button" class="btn btn-success">Read More</button>
+   
+    <i class="fa-sharp fa-solid fa-eye"></i> ${category.total_view ? category.total_view : '<span  fw-bold">No Viewer</span>'}
+   
     </div>
     <ul class="list-group list-group-flush  d-flex align-items-center">
        <img class="w-25 rounded-circle mt-2" src=${category.author.img} alt=""> 
-      <li class="list-group-item">${category.author.name}</li>
+      <li class="list-group-item">${category.author.name ? category.author.name : '<span  fw-bold">Not Found</span>'}</li>
       <li class="list-group-item">${category.author.published_date}</li>
     </ul>
     
@@ -87,21 +90,33 @@ const toggleSpiner= isLoading=>{
         lodarSection.classList.add('d-none')
     }
 }
-const a= 'https://openapi.programming-hero.com/api/news/category/01'
+
 catagoryNewsPage(01)
 
 
 loadNewsCatagory()
 
-function openNav() {
+function openNav(newsID) {
     document.getElementById("mySidenav").style.width = "100%";
+
+   const newsUrl=`https://openapi.programming-hero.com/api/news/${newsID}` 
+
+  fetch(newsUrl)
+  .then(res=>res.json())
+  .then(data=>displayFullNews(data.data[0]))
+
+  const displayFullNews= Newsdata=>{
+    console.log(Newsdata)
     const newsDetails= document.getElementById("mySidenav1")
     newsDetails.innerHTML=`
-   <h1> ${category.title}</h1>
-   <img src=${category.image_url} class="card-img-top w-50" alt="...">
-   <h6>0${category.details}</h6>
+   <h1> ${Newsdata.title}</h1>
+   <img src=${Newsdata.image_url} class="card-img-top w-50" alt="...">
+   <h6>${Newsdata.details}</h6>
     
     `
+  }
+
+   
   }
   
   function closeNav() {
